bf08b97106d342b9cfe097ade7226f8b1f1615e6
Silvio Traversaro <silvio@traversaro.it>
2022-05-27 21:39:28 +0200

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,14 @@
 mkdir build
 cd build
 
+:: is enable to backport
+:: https://github.com/tinyobjloader/tinyobjloader/pull/220
 cmake ^
     -G "Ninja" ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DBUILD_SHARED_LIBS:BOOL=ON ^
+    -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS:BOOL=ON ^
     %SRC_DIR%
 if errorlevel 1 exit 1
 
